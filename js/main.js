ba90a68ce8157ac3f0c73e0e51d660cdeab6d8c8
rastafarilife1 <99938291+rastafarilife1@users.noreply.github.com>
2022-02-25 12:35:24 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,1 @@
+const iconMenu=document.querySelector(".menu__icon"),menuBody=document.querySelector(".menu__body");iconMenu&&iconMenu.addEventListener("click",(function(e){iconMenu.classList.toggle("_active"),menuBody.classList.toggle("_active")}));
